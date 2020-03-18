898d0fd156d6b85045980c6ef7e485b66cd1ff15
timvq68551 <54223159+timvq68551@users.noreply.github.com>
2020-03-18 11:16:10 -0700

    Add files via upload

---
+++ M1_LA_Hello_World.swift
@@ -0,0 +1,29 @@
+/*:
+## Module 1, Lab A - Hello World
+
+The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
+*/
+
+print("I have learned the following:")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+
+/*:
+ Now it's your turn. Write a line of code that prints out your name
+ */
+
+
+/*:
+### Expected output:
+[Your name]
+*/
+/*:
+Once you have that working, you can try a little more. Use at least four print statements to output some lyrics to one of your favorite songs.
+*/
+
+
+/*:
+ ### Expected output:
+ [At least four lines of lyrics from a song!]
+ */
